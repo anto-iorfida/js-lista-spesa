97a59dfc5481024b25930a18cd5f6a90480e994a
anto-iorfida <antonioiorfida@hotmail.com>
2024-03-13 15:41:42 +0100

    feat completed es ciclo while

---
+++ ciclo-while/js/script.js
@@ -0,0 +1,37 @@
+// Data una lista della spesa:
+// const list = [
+//     'Latte',
+//     'Cacao',
+//     'Insalata',
+//     'Carote',
+//     'Pomodori',
+//     'Piatti pronti'
+// ];
+// Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+// Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.Consigli:
+// - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+// - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+
+// prendere la variabile assegnata 
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+
+// assegnare ad una variabile ul dell'html  
+let printList = document.querySelector('#list');
+
+// creare un ciclo while per lavorare e stampare ogni elemento 
+let i = 0
+while(i < list.length){
+
+    let listElement = list[i];
+    
+    printList.innerHTML += `<li>${listElement}</li>`
+
+    i++
+}
